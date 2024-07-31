c43153e109b7246599295c8a355cab58f32e58a7
rasuldadas@outlook.com <rasuldadas@outlook.com>
2024-07-31 18:32:09 +0400

    Layout

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,1 +1,17 @@
-document.getElementById("demo").innerHTML = "Salam millet";
+document.getElementById("js-demo").innerHTML = "This site for practicing!";
+document.getElementById("js-demo").style.fontSize = "30px";
+document.getElementById("js-demo").style.color = "blue";
+document.getElementById("js-demo").style.textAlign = "center";
+document.getElementById("js-demo").style.backgroundImage = "../Images/ai-image-generator-one.webp"
+document.getElementById("js-demo").style.marginBottom = "50px"
+
+
+function light(sw){
+    let pic;
+    if(sw == 0){
+        pic = "../Images/turn-off.jpg"
+    }else{
+        pic = "../Images/turn-on.jpg"
+    }
+    document.getElementById('bulbImage').src = pic;
+}
